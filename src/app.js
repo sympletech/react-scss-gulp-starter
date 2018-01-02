ea930513562d33de1ff810500b6c9411995da507
Daniel Lewis <daniel_lewis@esri.com>
2018-01-02 12:08:59 -0800

    added a seperate component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+import ComponentA from './app/components/component_a';
+
 ReactDOM.render(
-	<div>React Online</div>,
+	<div>
+		React Online!
+		<ComponentA />
+	</div>,
 	document.getElementById('root')
 );
