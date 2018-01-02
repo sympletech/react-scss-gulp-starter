ff90c2e57dd442ff9481f7905ffcdf975ae2abf0
Daniel Lewis <daniel_lewis@esri.com>
2018-01-02 11:59:53 -0800

    initial commit

---
+++ gulp-tasks/compile-js.js
@@ -0,0 +1,99 @@
+const gulp = require('gulp');
+const logger = require('gulp-logger');
+const watch = require('gulp-watch');
+const rollup_stream = require('rollup-stream');
+const buffer = require('vinyl-buffer');
+const rename = require('gulp-rename');
+const rollup_plugin_babel = require('rollup-plugin-babel');
+const babel_plugin_external_helpers = require('babel-plugin-external-helpers');
+const babel_preset_stage_0 = require('babel-preset-stage-0');
+const babel_preset_react = require('babel-preset-react');
+const rollup_plugin_node_resolve = require('rollup-plugin-node-resolve');
+const rollup_plugin_commonjs = require('rollup-plugin-commonjs');
+const rollup_plugin_uglify = require('rollup-plugin-uglify');
+const rollup_plugin_node_globals = require('rollup-plugin-node-globals');
+const babel_preset_env = require('babel-preset-env');
+const source = require('vinyl-source-stream');
+const sourcemaps = require('gulp-sourcemaps');
+
+const pkg = require('../package.json');
+
+function compileJs() {
+	compileJsFile({rootJs: `./${pkg.gulp_config.src_path}/app.js`, saveAs: `app.js`});
+}
+
+function compileJsFile({rootJs, saveAs}) {
+	rollup_stream({
+		input: rootJs,
+		format: 'iife',
+		name: pkg.gulp_config.build_name,
+		sourcemap: true,
+		plugins: [
+			rollup_plugin_node_resolve({
+				browser: true,
+				main: true
+			}),
+			rollup_plugin_commonjs({
+				exclude: 'node_modules/process-es6/**',
+				include: [
+					'node_modules/create-react-class/**',
+					'node_modules/fbjs/**',
+					'node_modules/object-assign/**',
+					'node_modules/react/**',
+					'node_modules/react-dom/**',
+					'node_modules/prop-types/**'
+				]
+			}),
+			rollup_plugin_babel({
+				babelrc: false,
+				exclude: 'node_modules/**',
+				plugins: [
+					babel_plugin_external_helpers
+				],
+				presets: [
+					[babel_preset_env, {
+						modules: false,
+						targets: {
+							"browsers": ['last 2 versions', 'Safari >6', 'IE > 10']
+						}
+					}],
+					babel_preset_stage_0,
+					babel_preset_react
+				]
+			}),
+			rollup_plugin_node_globals(),
+			rollup_plugin_uglify(false)
+		]
+	}).pipe(
+		source(`./${pkg.main}`)
+	).pipe(logger({
+		before: 'Compiling JS',
+		after: 'Compiling JS complete!',
+		showChange: false
+	})).pipe(
+		buffer()
+	).pipe(
+		sourcemaps.init({
+			loadMaps: true
+		})
+	).pipe(
+		rename(saveAs)
+	).pipe(
+		sourcemaps.write('.')
+	).pipe(
+		gulp.dest(pkg.gulp_config.build_path)
+	)
+}
+
+gulp.task('compile-js', () => {
+	compileJs();
+});
+
+gulp.task('watch-js', () => {
+	console.log('watching js...');
+	compileJs();
+	const jsGlob = `${pkg.gulp_config.src_path}/**/*.js`;
+	watch(jsGlob, () => {
+		compileJs();
+	});
+});
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+	<div>React Online</div>,
+	document.getElementById('root')
+);
